b09fdbf444eb4f018fcea19540a2ff20bd52516f
kingd <kingddd301@gmail.com>
2023-02-20 02:36:36 +0200

    Adding msgboard app details
    creating msgboard app, including view, model, url etc.

---
+++ msgboard/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils import timezone
+
+# To describe the structure of our application’s database. the Message-Table
+class Message(models.Model):
+    author = models.CharField(max_length=200)         # author field
+    text = models.TextField()                         # text field
+    date = models.DateTimeField(default=timezone.now) # date field
---
+++ msgboard/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Message
+
+def board(request):
+    messages = Message.objects.order_by('-date')
+    return render(request, 'msgboard/board.html', {'messages': messages})
